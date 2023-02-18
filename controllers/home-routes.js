9405bfbfe376ebef4b4d67d964a6e3226268da62
byxz885@gmail.com <53200767+byzESC@users.noreply.github.com>
2023-02-18 11:29:47 -0800

    fixing the project after graded

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -18,10 +18,10 @@ router.get("/", async (req, res) => {
     });
 
     const posts = postData.map((post) => post.get({ plain: true }));
-    // posts.forEach((post) => {
-    //   console.log(post)
-    //   console.log(post.comments)
-    // });
+    posts.forEach((post) => {
+      console.log(post)
+      console.log(post.comments)
+    });
     res.render('homepage', { posts, logged_in: req.session.logged_in });
   } catch (err) {
     res.status(500).json(err);
